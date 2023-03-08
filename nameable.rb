e04653ea4cadddd0705405b7ffe166dfcc6f3ecb
diego <dgcuenca@gmail.com>
2023-03-08 10:52:05 -0500

    Create base Decorator

--- nameable.rb
+++ nameable.rb
@@ -3,3 +3,15 @@ class Nameable
       raise NotImplementedError, "Subclasses must implement the correct_name method"
     end
 end
+
+class Decorator < Nameable
+    def initialize(nameable)
+      @nameable = nameable
+    end
+    
+    def correct_name
+      @nameable.correct_name
+    end
+  end
+
+  
