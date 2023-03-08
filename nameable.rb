eac1a69215a9794b273a24facc4501f7d9cb5799
diego <dgcuenca@gmail.com>
2023-03-08 10:55:27 -0500

    Create classes that inherits from base Decorator

--- nameable.rb
+++ nameable.rb
@@ -14,4 +14,14 @@ class Decorator < Nameable
     end
   end
 
+  class CapitalizeDecorator < Decorator
+    def correct_name
+      @nameable.correct_name.capitalize
+    end
+  end
   
+  class TrimmerDecorator < Decorator
+    def correct_name
+      @nameable.correct_name[0..9]
+    end
+  end
