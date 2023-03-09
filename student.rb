091939c93a0cd2f03cb7d21e56711a11ac6e6d06
diego <dgcuenca@gmail.com>
2023-03-09 08:43:43 -0500

    Create has-many/belongs-to between Classroom/Stude

--- student.rb
+++ student.rb
@@ -1,9 +1,11 @@
 require './person'
+require './classroom'
 
 class Student < Person
   def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
     @classroom = classroom
+    @classroom.add_student(self) unless @classroom.nil?
   end
 
   def play_hooky
