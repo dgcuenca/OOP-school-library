b3f50f1a06c610cca8df49126e823c8aa1a9f3d5
diego <dgcuenca@gmail.com>
2023-03-10 17:43:53 -0500

    Improve user experience when there arent books

--- app.rb
+++ app.rb
@@ -60,36 +60,41 @@ def list_all_books(books)
   end
 
   def create_rental(books, people, rentals)
-    puts 'Select a book from the following list by number'
-    books.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
-  
-    book_selected = nil
-    loop do
-      book_selected = gets.chomp.to_i
-      break if book_selected >= 0 && book_selected < books.length
-      puts "Invalid selection. Please enter a number between 0 and #{books.length - 1}"
-    end
-  
-    puts 'Select a person from the following list by number (not id)'
-    people.each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-  
-    person_selected = nil
-    loop do
-      person_selected = gets.chomp.to_i
-      break if person_selected >= 0 && person_selected < people.length
-      puts "Invalid selection. Please enter a number between 0 and #{people.length - 1}"
-    end
-  
-    puts 'Date: '
-    date = gets.chomp
-  
-    rental = Rental.new(date: date, person: people[person_selected], book: books[book_selected])
-    rentals << rental
-    puts 'Rental created successfully'
-    rental
+    if ( books.empty?)
+      puts 'There isnt books to rent, come back later'
+    else
+      puts 'Select a book from the following list by number'
+      books.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
+    
+      book_selected = nil
+      loop do
+        book_selected = gets.chomp.to_i
+        break if book_selected >= 0 && book_selected < books.length
+        puts "Invalid selection. Please enter a number between 0 and #{books.length - 1}"
+      end
+    
+      puts 'Select a person from the following list by number (not id)'
+      people.each_with_index do |person, index|
+        puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    
+      person_selected = nil
+      loop do
+        person_selected = gets.chomp.to_i
+        break if person_selected >= 0 && person_selected < people.length
+        puts "Invalid selection. Please enter a number between 0 and #{people.length - 1}"
+      end
+    
+      puts 'Date: '
+      date = gets.chomp
+    
+      rental = Rental.new(date: date, person: people[person_selected], book: books[book_selected])
+      rentals << rental
+      puts 'Rental created successfully'
+      rental
+     end
   end
+  
   
   def list_all_rentals_by_id(rentals)
     puts 'Enter the ID of the person:'
